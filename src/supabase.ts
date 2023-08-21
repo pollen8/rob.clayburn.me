aa5ac196e4915ae785d3419c5412428c37ed0024
Robert Clayburn <rob@infosum.com>
2023-08-22 00:42:11 +0100

    ack token

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types/supabase';
 
 const supabaseUrl = 'https://yjjjuiwzmgymfxdukddv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqamp1aXd6bWd5bWZ4ZHVrZGR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4Mjk4NzEsImV4cCI6MjAwNjQwNTg3MX0.w9mGVXE1ehDpjX_y_12U6BazuD80MfTt4OvJ3RIG85Y';
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const supabase = createClient<Database>(supabaseUrl, (process.env as any).supabaseToken)
+console.log(supabase);
 export type JournalRow = Database['public']['Tables']['journal']['Row'];
